22bb59a6f7eac2cf1b74508adbaaeb717e599b86
AntonS2002 <anton.shkarnikov@mail.ru>
2026-02-10 22:03:39 +0300

    Сделал регистрацию пользователя, нужно поправить поле номера телефона

--- src/type/index.ts
+++ src/type/index.ts
@@ -33,17 +33,7 @@ export interface UserRegistration {
     phoneNumber: string;
 }
 
-export interface Profile {
-    id: number;
-    username: string;
-    email: string;
-    date: string;
-    isBlocked: boolean;
-    roles: Role[];
-    phoneNumber: string;
-}
 
-type Role = ADMIN | USER | MODERATOR
 
 
 
