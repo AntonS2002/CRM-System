58cb399bfcd0349b0d7d85534f0bb872b0ee1afd
AntonS2002 <anton.shkarnikov@mail.ru>
2026-05-14 22:31:14 +0300

    feat: апгрейд дашборда, добавил диаграмму, добавил панель KPI, обновил хлебные крошки

--- src/pages/DashboardKPIpage.tsx
+++ src/pages/DashboardKPIpage.tsx
@@ -1,5 +1,6 @@
-import {Radio, type RadioChangeEvent, Select, Space, Tabs, type TabsProps} from "antd";
+import {Breadcrumb, Card, Col, Radio, type RadioChangeEvent, Row, Select, Space, Statistic, type TabsProps} from "antd";
 import {useState} from "react";
+import {CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
 
 export const DashboardKPIpage = () => {
     const [tabPlacement, setTabPlacement] = useState<TabsProps['tabPlacement']>('top');
@@ -8,9 +9,17 @@ export const DashboardKPIpage = () => {
         setTabPlacement(e.target.value);
     };
 
-    const onChange = (key: string) => {
-        console.log(key);
-    };
+    const data = [
+        {name: 'Vasya', age: 32},
+        {name: 'WQE', age: 31},
+        {name: 'dsgrs', age: 22},
+        {name: 'Fyjyjr5', age: 44},
+        {name: 'FXAWED', age: 35},
+        {name: 'FXAWED', age: 35},
+        {name: 'FXAWED', age: 35},
+        {name: 'FXAWED', age: 35},
+        {name: 'FXAWED', age: 35},
+    ]
 
     return (
         <div style={{margin: '3rem'}}>
@@ -34,23 +43,55 @@ export const DashboardKPIpage = () => {
                 />
             </div>
             <div>
-                <Tabs
-                    size="large"
-                    onChange={onChange}
-                    type="card"
-                    items={Array.from({ length: 3 }).map((_, i) => {
-                        const id = String(i + 1);
-                        return {
-                            label: `Tab ${id}`,
-                            key: id,
-                            children: `Content of Tab Pane ${id}`,
-                        };
-                    })}
+                <Breadcrumb
+                    separator=">"
+                    style={{fontSize: '1rem', display: 'flex', justifyContent: 'center'}}
+                    items={[
+                        {
+                            title: 'Холдинг',
+                            href: '',
+                        },
+                        {
+                            title: 'Предприятие',
+                            href: '',
+                        },
+                        {
+                            title: 'Площадка',
+                            href: '',
+                        },
+                        {
+                            title: 'Цех',
+                            href: '',
+                        },
+                    ]}
                 />
             </div>
-            <div>
-                <div style={{width: '600px', height: '300px', backgroundColor: '#ffc1c1'}}>
-
+            <div style={{margin: '1rem', display: 'flex', justifyContent: 'center', gap: '7rem'}}>
+                <div style={{width: '800px', height: '400px'}}>
+                    <Card title="Карточка KPI" variant="borderless" style={{ width: '100%', height: '100%' }}>
+                        <Row>
+                            <Col span={12}>
+                                <Statistic title="OEE:" value={112893} />
+                                <Statistic title="Маржинальность:" value={112893} />
+                                <Statistic title="Загрузка мощностей:" value={112893} />
+                            </Col>
+                            <Col span={12}>
+                                <Statistic title="Выработка:" value={112893} />
+                                <Statistic title="Простои:" value={112893} />
+                            </Col>
+                        </Row>
+                    </Card>
+                </div>
+                <div style={{width:'800px', height: '400px'}}>
+                    <ResponsiveContainer width="100%" height='100%'>
+                        <LineChart data={data}>
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis dataKey="age" />
+                            <Tooltip />
+                            <Line type="monotone" dataKey="age" stroke="#1677ff" />
+                        </LineChart>
+                    </ResponsiveContainer>
                 </div>
             </div>
 
